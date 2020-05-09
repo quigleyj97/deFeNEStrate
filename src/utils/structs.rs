c90b582e5d56862d27f56bc603ba5cb9468a1715
Joe Quigley <quigley.joseph@outlook.com>
2020-05-08 17:45:58 -0700

    Change: Make CPU use new bus

--- src/utils/structs.rs
+++ src/utils/structs.rs
@@ -1,6 +1,103 @@
 //! Helper structs for emulation
 
 pub mod cpu {
+    /// A struct holding state information about a 6502 CPU.
+    ///
+    /// This struct is held internally, but can be copied to power to things
+    /// like debug formatters and, if taken at the end of a simulation cycle,
+    /// serialization.
+    #[derive(Debug, Copy, PartialEq)]
+    pub struct CpuState {
+        /// The Accumulator register
+        pub acc: u8,
+
+        /// X index register
+        pub x: u8,
+
+        /// Y index register
+        pub y: u8,
+
+        /// The stack pointer
+        ///
+        /// # Note
+        ///
+        /// This register is a pointer to a location in memory on the first page
+        /// ($01XX) of memory. The 6502 uses a bottom-up stack, so the 'first'
+        /// location on the stack is `$01FF` and the 'last' is `$0100`.
+        ///
+        /// Stack _overflow_ occurs when the stack pointer decreases all the way to
+        /// $00 and wraps around to $FF (the beginning). _Underflow_ occurs the
+        /// other way around, from $FF to $00.
+        pub stack: u8,
+
+        /// The program counter
+        ///
+        /// # Note
+        ///
+        /// This is incremented by the emulator after executing each instruction,
+        /// and refers to the address in memory of the next instruction
+        pub pc: u16,
+
+        /// The instruction being executed.
+        ///
+        /// # Note
+        ///
+        /// Instructions consist of an opcode, having 1 byte, and an optional
+        /// operand having 1 or 2 bytes (depending on the instruction and addressing
+        /// mode).
+        ///
+        /// The last 8 bits of this register are unused.
+        pub instruction: u32,
+
+        /// The program status register.
+        pub status: Status,
+
+        /// The total number of cycles that this CPU has ran
+        ///
+        /// # Note
+        ///
+        /// This is allowed to overflow, as it's only used for debugging and test
+        /// comparison. It is not a part of core emulation.
+        pub tot_cycles: u32,
+
+        /// The resolved address of the instruction
+        pub addr: u16,
+
+        /// The addressing mode of the opcode being executed
+        pub addr_mode: AddressingMode,
+
+        /// The opcode being executed
+        pub instr: Instruction,
+    }
+
+    impl CpuState {
+        /// Create a new CpuState
+        ///
+        /// # Note
+        ///
+        /// Default values are the NES power-up vals
+        /// cf. http://wiki.nesdev.com/w/index.php/CPU_power_up_state
+        pub fn new() -> CpuState {
+            CpuState {
+                acc: 0,
+                x: 0,
+                y: 0,
+                stack: 0xFD,
+                pc: 0xC000,
+                // IRQ disabled
+                // Unwrapping b/c this is a constant and should be OK
+                status: Status::from_bits(0x24).unwrap(),
+
+                // internal state
+                tot_cycles: 7,
+                instruction: 0xEA,
+                addr: 0,
+                addr_mode: AddressingMode::Impl,
+                instr: Instruction::NOP,
+            }
+        }
+    }
+
     #[derive(Debug, PartialEq)]
     pub enum AddressingMode {
         /// Zero-Page
